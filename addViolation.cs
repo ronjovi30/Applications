936a31653ab5f001f90ee36fe1fa5531a43f98e4
ronjovi30 <rjmletriro@iskolarngbayn.pup.edu.ph>
2021-05-30 17:01:14 +0800

    Responsibility and Relationship

---
+++ addViolation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class addViolation
+
+    {
+
+        public int addingViolation(string studentName, string studentCourse, int numberViolation)
+        {
+            Console.WriteLine("| |_____________________________________| |");
+            Console.WriteLine("| |                                     | |");
+            Console.WriteLine("| | STUDENT VIOlATION RECORD MANAGEMENT | |");
+            Console.WriteLine("| |_____________________________________| |");
+            Console.WriteLine("| |                                     | |\n\n\n");
+
+            Console.Write("\nPosible Violation\n1. no ID\n2. improper dressing\n3. engaging in any unlawful Activity\n\n\nnote: type only the number correspoding to the violation.\n\n");
+            Console.Write("Violation: ");
+            String violation = Console.ReadLine();
+
+            Console.WriteLine("\n\n\nVoilation has been added\n");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("| |_____________________________________| |");
+            Console.WriteLine("| |                                     | |");
+            Console.WriteLine("| | STUDENT VIOlATION RECORD MANAGEMENT | |");
+            Console.WriteLine("| |_____________________________________| |");
+            Console.WriteLine("| |                                     | |\n\n\n");
+
+            Console.WriteLine("Name:" + studentName);
+            Console.WriteLine("Course:" + studentCourse);
+
+            // condition
+
+            if (violation == "1")
+            {
+
+                Console.WriteLine("Violation: No ID");
+                Console.WriteLine("Number of violation: " + numberViolation + 1);
+
+            }
+            else if (violation == "2")
+            {
+
+                Console.WriteLine("Violation: Improper Dressing ");
+                Console.WriteLine("Number of violation: " + numberViolation + 1);
+
+            }
+            else if (violation == "3")
+            {
+
+                Console.WriteLine("Violation: Engaging in any unlawful Activity ");
+                Console.WriteLine("Number of violation: " + numberViolation + 1);
+
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+            Console.ReadKey();
+            return numberViolation + 1;
+
+        }
+
+
+    }
+}
---
+++ search.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class search
+    {
+        public void searchFunction(List<string> course, List<string> bsa1S1, List<string> bsa2S1, List<string> bsa3S1, List<string> bsa4S1, List<int> bsa1S1N, List<int> bsa2S1N, List<int> bsa3S1N, List<int> bsa4S1N)
+        {
+            Console.WriteLine("| |_____________________________________| |");
+            Console.WriteLine("| |                                     | |");
+            Console.WriteLine("| | STUDENT VIOlATION RECORD MANAGEMENT | |");
+            Console.WriteLine("| |_____________________________________| |");
+            Console.WriteLine("| |                                     | |\n\n\n");
+
+            Console.Write("Name: ");
+            String name = Console.ReadLine();
+            Console.Clear();
+            //BSA
+            if (bsa1S1.Contains(name))
+            {
+                Console.WriteLine("| |_____________________________________| |");
+                Console.WriteLine("| |                                     | |");
+                Console.WriteLine("| | STUDENT VIOlATION RECORD MANAGEMENT | |");
+                Console.WriteLine("| |_____________________________________| |");
+                Console.WriteLine("| |                                     | |\n\n\n");
+
+                Console.WriteLine("Name: " + bsa1S1[bsa1S1.IndexOf(name)]);
+                Console.WriteLine("Course: " + course[1]);
+                Console.WriteLine("Number of Violation: " + bsa1S1N[bsa1S1.IndexOf(name)]);
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else if (bsa2S1.Contains(name))
+            {
+                Console.WriteLine("| |_____________________________________| |");
+                Console.WriteLine("| |                                     | |");
+                Console.WriteLine("| | STUDENT VIOlATION RECORD MANAGEMENT | |");
+                Console.WriteLine("| |_____________________________________| |");
+                Console.WriteLine("| |                                     | |\n\n\n");
+
+                Console.WriteLine("Name: " + bsa2S1[bsa2S1.IndexOf(name)]);
+                Console.WriteLine("Course: " + course[1]);
+                Console.WriteLine("Number of Violation: " + bsa2S1N[bsa2S1.IndexOf(name)]);
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else if (bsa3S1.Contains(name))
+            {
+                Console.WriteLine("| |_____________________________________| |");
+                Console.WriteLine("| |                                     | |");
+                Console.WriteLine("| | STUDENT VIOlATION RECORD MANAGEMENT | |");
+                Console.WriteLine("| |_____________________________________| |");
+                Console.WriteLine("| |                                     | |\n\n\n");
+
+                Console.WriteLine("Name: " + bsa3S1[bsa3S1.IndexOf(name)]);
+                Console.WriteLine("Course: " + course[1]);
+                Console.WriteLine("Number of Violation: " + bsa3S1N[bsa3S1.IndexOf(name)]);
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else if (bsa4S1.Contains(name))
+            {
+                Console.WriteLine("| |_____________________________________| |");
+                Console.WriteLine("| |                                     | |");
+                Console.WriteLine("| | STUDENT VIOlATION RECORD MANAGEMENT | |");
+                Console.WriteLine("| |_____________________________________| |");
+                Console.WriteLine("| |                                     | |\n\n\n");
+
+                Console.WriteLine("Name: " + bsa4S1[bsa4S1.IndexOf(name)]);
+                Console.WriteLine("Course: " + course[1]);
+                Console.WriteLine("Number of Violation: " + bsa4S1N[bsa4S1.IndexOf(name)]);
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("Name is not found.");
+                Console.Clear();
+            }
+        }
+    }
+}
